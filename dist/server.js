4389491d7393ff4d871931847fcb06d98c5bd523
Nur Uddin <nuruddinnur538@gmail.com>
2025-07-26 09:01:56 +0600

    setup completed

---
+++ dist/server.js
@@ -0,0 +1,75 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const app_1 = __importDefault(require("./app"));
+let server;
+const startServer = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        yield mongoose_1.default.connect("mongodb+srv://nuruddin538:ABab12tour@cluster0.1hnnpzf.mongodb.net/tour-management-backend");
+        console.log("Connected to DB!!");
+        server = app_1.default.listen(5000, () => {
+            console.log("Server is listening to port 5000");
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
+startServer();
+process.on("SIGTERM", () => {
+    console.log("SIGTERM signal received... Server shutting down..");
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        });
+    }
+    process.exit(1);
+});
+process.on("SIGINT", () => {
+    console.log("SIGINT signal received... Server shutting down..");
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        });
+    }
+    process.exit(1);
+});
+process.on("unhandledRejection", (err) => {
+    console.log("Unhandled Rejection detected... Server shutting down..", err);
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        });
+    }
+    process.exit(1);
+});
+process.on("uncaughtException", (err) => {
+    console.log("Uncaught Exception detected... Server shutting down..", err);
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        });
+    }
+    process.exit(1);
+});
+// Unhandled rejection error
+// Promise.reject(new Error("I forgot to catch this promise"));
+// Uncaught Exception Error
+// throw new Error("I forgot to handle this local error");
+/**
+ * unhandled rejection error
+ * uncaught rejection error
+ * signal termination sigterm
+ */
